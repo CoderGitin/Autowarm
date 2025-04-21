fbbe202d92c4c1b52d7002727c2203f68707c676
Gitin <gitin82@gmail.com>
2025-04-21 13:18:06 +0530

    autowarmm

---
+++ my-app/src/app/components/Playbutton.tsx
@@ -0,0 +1,27 @@
+const PlayButton = () => {
+    return (
+      <svg
+        width="6"
+        height="5"
+        viewBox="0 0 7 9"
+        fill="blue"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-7 h-9 fill-current text-blue-500"
+      >
+        <g clipPath="url(#clip0_107_780)">
+          <path
+            d="M6.53735 4.05987L0.537354 7.52397L0.537354 0.595764L6.53735 4.05987Z"
+            fill="currentColor"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_107_780">
+            <rect width="5" height="7" fill="blue" transform="translate(0.537354 0.0598755)" />
+          </clipPath>
+        </defs>
+      </svg>
+    );
+  };
+  
+  export default PlayButton;
+  
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Homepg from "./components/Homepg";
+import Homepg2 from "./components/Homepg2";
+import Homepg4 from "./components/Homepg4";
+import Pricep from "./components/price"
+import Contact from "./components/Contact";
+
+
+export default function Home() {
+  return (
+  <>
+  <Homepg/>
+  <Pricep/>
+  <Homepg2/>
+  <Homepg4/>
+  <Contact/>
+  
+  </> 
+  
+);
+}
